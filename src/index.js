a0dc16109eae2e2baedbeada6138d6a5d819f513
subhojitsarkar <ssubhojit1997@gmail.com>
2024-04-24 17:28:43 +0530

    setup files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+import express from 'express'
+// const dotenv = require('dotenv');
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
